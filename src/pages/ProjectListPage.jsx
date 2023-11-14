bde6a067f8e18794a9b2f467df72034531eb0d2e
AnaRibeiroPT <anaribeiro924@gmail.com>
2023-11-14 14:40:34 +0000

    delete and other functionalities

--- src/pages/ProjectListPage.jsx
+++ src/pages/ProjectListPage.jsx
@@ -1,31 +1,37 @@
-import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
 import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const API_URL = "https://project-management-api-4641927fee65.herokuapp.com";
 
 function ProjectListPage() {
   const [projects, setProjects] = useState([]);
 
   const getAllProjects = () => {
     axios
-      .get(
-        "https://project-management-api-4641927fee65.herokuapp.com/projects?_embed=tasks"
-      )
+      .get(API_URL + "/projects?_embed=tasks")
       .then((response) => {
         setProjects(response.data);
       })
       .catch((error) => {
-        console.log("Something went wrong...");
+        console.log("Error getting projects from the API...");
         console.log(error);
       });
   };
-  
+
   useEffect(() => {
     getAllProjects();
   }, []);
 
   return (
     <div className="ProjectListPage">
-      <h1>List of Projects</h1>
+      <h2>List of projects:</h2>
+
+      <Link to="/projects/create">
+        <p>
+          <button>Create Project</button>
+        </p>
+      </Link>
 
       {projects.map((project) => {
         return (
